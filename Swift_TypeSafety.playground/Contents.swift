5932df9e5e8ee1b46e63c918e6e9d1c79f72284f
Techtraveler <iamtechtraveler@gmail.com>
2023-06-20 00:00:41 +0200

    type safety

---
+++ Swift_TypeSafety.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// Swift is a type safe programming language
+
+// Type safety
+
+var userId = 9712468214
+// userId = "Hans"
+// userId = 10.5
+
+// it cannot be assigned value of another type to another type
+// String to int or int to string or double to String
+
+
